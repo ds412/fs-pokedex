9858982a5a4b9458e1033533462a2b527eb17e87
Dennis Seeldrayers <dennis.seeldrayers@gmail.com>
2026-07-22 17:15:39 -0400

    CI/CD: exercise 9 - Added an end-to-end test using Playwright

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@playwright/test')
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  webServer: {
+    command: 'npm run start-e2e-test',
+    url: 'http://localhost:8080',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: 'http://localhost:8080',
+  },
+})
