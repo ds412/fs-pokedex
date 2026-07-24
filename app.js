ed3111a40de8542981800bf3972e75ee80e2bae0
Dennis Seeldrayers <dennis.seeldrayers@gmail.com>
2026-07-24 19:14:47 -0400

    CI/CD exercise 17: Added linter error to check if main branch is protected

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5001
 
-app.use(express.static('dist'))
+app.use(express.static("dist"))
 
 app.get('/health', (req, res) => {
   res.send('ok')
